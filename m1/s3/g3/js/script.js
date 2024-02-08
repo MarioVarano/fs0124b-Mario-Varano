1671a3eaaf8cc4c99d41c0112393e50af0fb3b61
MarioVarano <alexxan@live.it>
2024-02-08 16:56:54 +0100

    esercizio_s3_g4

--- m1/s3/g3/js/script.js
+++ m1/s3/g3/js/script.js
@@ -1,9 +1,22 @@
-const invia = document.querySelector('.aggiungi')
-const text = document.querySelector('.testo')
-invia.addEventListener('click', () => {
-    let nuovo = document.createElement('div')
-    nuovo.innerText = text.value
-    let target = document.querySelector('.target')
-    target.appendChild(nuovo)
+console.log('ciao')
+
+const testo = document.querySelector('#testo')
+const bottone = document.querySelector('#aggiungi')
+const target = document.querySelector('#target')
+
+bottone.addEventListener('click',function(){
+    let li = document.createElement('li')
+    li.innerText = testo.value 
+
+
+    let bottone = document.createElement('button')
+    bottone.innerText = 'Elimina'
+    bottone.addEventListener('click',function(){
+        li.remove()
+    })
+
+    li.append(bottone)
+    target.append(li)
+
     
 })
---
+++ m1/s3/g4/js/script.js
@@ -0,0 +1,25 @@
+const estratti = []
+
+const target = document.querySelector('.caselle')
+for(let i =1; i<=76; i++){
+    const casella = document.createElement('div')
+    casella.classList.add('singola')
+    casella.innerText = i
+
+target.append(casella)
+}
+
+const bottone = document.querySelector('.estrai-random')
+bottone.addEventListener('click' ,function(){
+    const numeroEstratto = Math.floor(Math.random() * 76 ) + 1 
+
+
+    
+    if(casella.innerText == Number(numeroEstratto)){
+        casella.classList.add('selezionata')
+    }
+
+
+})
+
+
