794f90d8af5e2d8b7cf5c9ec1c52e97a89b34735
MarioVarano <alexxan@live.it>
2024-03-19 15:07:57 +0100

    angular1

---
+++ m3/s1/g2/esercizio-angular/src/app/componenti/secondo/secondo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondo',
+  templateUrl: './secondo.component.html',
+  styleUrl: './secondo.component.scss'
+})
+export class SecondoComponent {
+
+}
