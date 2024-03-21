5175e523f0439e47251f5feb80c9aca1f2354d47
MarioVarano <alexxan@live.it>
2024-03-21 16:49:53 +0100

    esercizio

--- m3/s1/g3/esercizio/src/app/app-routing.module.ts
+++ m3/s1/g3/esercizio/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActiveComponent } from './pagine/active/active.component';
+import { InactiveComponent } from './pagine/inactive/inactive.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'active',
+    component:ActiveComponent
+  },
+  {
+    path:'inactive',
+    component:InactiveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s1/g3/esercizio/src/app/app.module.ts
+++ m3/s1/g3/esercizio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componenti/nav/nav.component';
+import { ActiveComponent } from './pagine/active/active.component';
+import { InactiveComponent } from './pagine/inactive/inactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ActiveComponent,
+    InactiveComponent
   ],
   imports: [
     BrowserModule,
