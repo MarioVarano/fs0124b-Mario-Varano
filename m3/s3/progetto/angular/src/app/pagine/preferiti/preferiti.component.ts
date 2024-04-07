7a777ea319135ca96a8dc7d497caa3e6ed98a42a
MarioVarano <alexxan@live.it>
2024-04-07 23:56:21 +0200

    aggiorno json ma non visualizzo

--- m3/s3/progetto/angular/src/app/pagine/preferiti/preferiti.component.ts
+++ m3/s3/progetto/angular/src/app/pagine/preferiti/preferiti.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { IFilm } from '../../Modules/i-film';
+import { AuthService } from '../../auth/auth.service';
+import { FilmService } from '../film.service';
+import { IUser } from '../../Modules/i-user';
 
 @Component({
   selector: 'app-preferiti',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './preferiti.component.scss'
 })
 export class PreferitiComponent {
+  preferitiArr:IFilm[] = []
+  var!:IUser|null
+
+
+  constructor(private filmSvc:FilmService,
+    private authSVC:AuthService
+  ){}
+
+  ngOnInit(){
+    this.authSVC.user$.subscribe(res => this.var = res)
+    this.preferitiArr = this.var?.preferiti || []
+    console.log(this.preferitiArr);
+
+
+
+  }
+
 
 }
