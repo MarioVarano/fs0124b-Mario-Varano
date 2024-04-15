9573101579711cea2774d6ddf96b7d2fe191f786
MarioVarano <alexxan@live.it>
2024-04-15 17:05:11 +0200

    esercizio1

---
+++ backend/m1/s1/g1/javaa/src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int risultato = moltiplicazione(20,4);
+        String risultato2 = concatena(1, "ciao");
+
+        System.out.println(risultato);
+        System.out.println(risultato2);
+
+
+    }
+
+
+    public static int moltiplicazione(int a, int b){
+        int c = a*b;
+        return c;
+    }
+
+    public static String concatena(int a, String b){
+        String c = a + b;
+        return c;
+    }
+}
